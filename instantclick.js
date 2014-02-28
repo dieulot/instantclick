dieulot/instantclick

commit f89abd5f646f536ad459a106c3535a16d8ccb5f8

Progress bar adjusts its size for mobile devices + config via CSS
The progress bar adjusts its size and position, so that you don't need to optimize your website for mobile for it to work correctly.

The progress bar configuration is now done by CSS instead of another argument in `InstantClick.init`.

instantclick.js
@@ -55,9 +55,6 @@ var InstantClick = function(document, location) {
   }
 
   function changePage(title, body, newUrl, scrollY) {
-    if (bar.enabled) {
-      body += $barContainer.outerHTML
-    }
     var doc = document.implementation.createHTMLDocument('')
     doc.documentElement.innerHTML = body
     document.documentElement.replaceChild(doc.body, document.body)
@@ -381,27 +378,14 @@ var InstantClick = function(document, location) {
         $barElement,
         $barTransformProperty,
         $barProgress,
-    $barTimer,
-    $barIncTo
+        $barTimer
 
-    function init(color) {
-      if (!color) {
-        return
-      }
-      /* Some colors:
-         Default  : #29f
-         NProgress: #29d (2px)
-         zprogress: #1c88ff (3px)
-         Chrome on iPhone (iOS 6): #4e90fe (4px)
-         Safari on iPad, iOS 7: #007aff (2.5px)
-      */
-
-      enabled = true
-
+    function init() {
       $barContainer = document.createElement('div')
-      $barContainer.id = 'ic-progress'
+      $barContainer.id = 'instantclick'
       $barElement = document.createElement('div')
-      $barElement.id = 'ic-progress-bar'
+      $barElement.id = 'instantclick-bar'
+      $barElement.className = 'instantclick-bar'
       $barContainer.appendChild($barElement)
 
       var vendors = ['Webkit', 'Moz', 'O']
@@ -425,15 +409,22 @@ var InstantClick = function(document, location) {
       }
 
       var style = document.createElement('style')
-      style.innerHTML = '#ic-progress { position:fixed; top:0; left:0; width: 100%; pointer-events:none; z-index:3000; ' + transitionProperty + ':opacity .25s .1s; }'
-        + '#ic-progress-bar { background:' + color + '; width:100%; margin-left:-100%; height:3px; ' + transitionProperty + ':all .25s; }'
+      style.innerHTML = '#instantclick{position:absolute;top:0;left:0;width:100%;pointer-events:none;z-index:3000;' + transitionProperty + ':opacity .25s .1s}'
+        + '.instantclick-bar{background:#29f;width:100%;margin-left:-100%;height:3px;' + transitionProperty + ':all .25s}'
+      /* We set the bar's background in `.instantclick-bar` so that it can be
+         overriden in CSS with `#instantclick-bar`, as IDs have higher priority.
+      */
       document.head.appendChild(style)
+
+      if ('orientation' in window) {
+        updatePositionAndScale()
+        addEventListener('resize', updatePositionAndScale)
+        addEventListener('scroll', updatePositionAndScale)
+      }
+
     }
 
     function start(at, jump) {
-      if (!enabled) {
-        return
-      }
       $barProgress = at
       if (document.getElementById($barContainer.id)) {
         document.body.removeChild($barContainer)
@@ -476,14 +467,12 @@ var InstantClick = function(document, location) {
     }
 
     function done() {
-      if (!enabled) {
-        return
-      }
       if (document.getElementById($barContainer.id)) {
         clearTimeout($barTimer)
         $barProgress = 100
         update()
         $barContainer.style.opacity = '0'
+        /* If you're debugging, setting this to 0.5 is handy. */
         return
       }
 
@@ -494,13 +483,17 @@ var InstantClick = function(document, location) {
       /* Must be done in a timer, otherwise the CSS animation doesn't happen. */
     }
 
-    var enabled = false
+    function updatePositionAndScale() {
+      $barContainer.style.left = pageXOffset + 'px'
+      $barContainer.style.width = innerWidth + 'px'
+      $barContainer.style.top = pageYOffset + 'px'
+      $barContainer.style[$barTransformProperty] = 'scaleY(' + (innerWidth / screen[Math.abs(orientation) == 90 ? 'height' : 'width'])  + ')'
+    }
 
     return {
       init: init,
       start: start,
-      done: done,
-      enabled: enabled
+      done: done
     }
   }()
 
@@ -550,7 +543,6 @@ var InstantClick = function(document, location) {
       triggerPageEvent('change', true)
       return
     }
-    var barColor = '#29f'
     for (var i = arguments.length - 1; i >= 0; i--) {
       var arg = arguments[i]
       if (arg === true) {
@@ -561,11 +553,6 @@ var InstantClick = function(document, location) {
       }
       else if (typeof arg == 'number') {
         $delayBeforePreload = arg
-      }
-      else if (typeof arg == 'object') {
-        if ('progressBar' in arg) {
-          barColor = arg.progressBar
-        }
       }
     }
     $currentLocationWithoutHash = removeHash(location.href)
@@ -579,7 +566,7 @@ var InstantClick = function(document, location) {
 
     instantanize(true)
 
-    bar.init(barColor)
+    bar.init()
 
     triggerPageEvent('change', true)
 

tests/index.php
@@ -6,8 +6,6 @@ $pages = array(
   'Page without title' => 'no-title',
   'Minimal markup' => 'minimal',
   'NProgress' => 'nprogress',
-  'Orange progress bar' => 'orange-pb',
-  'No progress bar' => 'no-pb',
 );
 
 $page = 'welcome';
@@ -44,8 +42,10 @@ if (isset($_GET['wait'])) {
 <?php if ($page != 'no-title'): ?>
 <title><?php echo date('H : i : s') ?> . <?php printf("%03d", microtime() * 1000) ?></title>
 <?php endif ?>
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="style.css?<?php echo $append ?>">
+<?php if (1): ?>
 <meta name="viewport" content="width=600">
+<?php endif ?>
 
 <?php if ($page == 'nprogress'): ?>
 <link rel="stylesheet" href="vendors/nprogress/nprogress-0.1.2.css">
@@ -75,8 +75,6 @@ if (isset($_GET['wait'])) {
     <td><a href="?<?php echo ($row != '' ? ('page=' . $row) : '') . $append ?>"><?php echo $name ?></a>
 <?php if (in_array($row, array(
  'nprogress',
- 'orange-pb',
- 'no-pb',
 ))): ?>
         <a data-no-instant href="?page=<?php echo $row . $append ?>">↻</a>
 <?php endif ?>
@@ -129,21 +127,12 @@ InstantClick.on('change', function(isInitialLoad) {
 })
 
 InstantClick.init(<?php
-$comma = false;
 if ($preload_on == 'mousedown') {
   echo "'mousedown'";
-  $comma = true;
 }
 elseif ((int)$preload_on != 0) {
   echo $preload_on;
-  $comma = true;
 }
 
-if (in_array($page, array('orange-pb', 'no-pb'))) {
-   if ($comma) {
-     echo ", ";
-   }
-   echo "{progressBar: " . ($page == 'no-pb' ? "false" : "'orange'") . "}";
-}
 ?>);
 </script>

tests/no-pb.html
@@ -1,3 +1,0 @@
-<h1>No progress bar</h1>
-
-<p>Needs a refresh to be disabled.

tests/orange-pb.html
@@ -1,3 +1,0 @@
-<h1>Orange progress bar</h1>
-
-<p>Needs a refresh to appear.

tests/style.css
@@ -26,3 +26,11 @@ body {
 #divDebug hr {
 	margin: .2em 0;
 }
+
+/*#instantclick {
+	display: none;
+}*/
+
+/*#instantclick-bar {
+	background: red;
+}*/

tests/welcome.html
@@ -1,3 +1,7 @@
 <h2>Index page</h2>
 
 <a href="?<?php echo $append ?>"><b><i>A link with childs</i></b></a>
+
+<p style="font-size: 13px; width: 180px;">Some tiny text. Tiny text. Tiny text. Tiny text!!! I’m so tinyyyy!!!!!<br>
+This is useful to test the progress bar’s behavior when zoomed on a touch device.<br>
+Be sure to test with and without a viewport.</p>

commit 3c403a7ebe729e45c65246ce4a69c256e576ac91

Fix regression: Progress bar not showing after scroll
Introduced in f89abd5f646f536ad459a106c3535a16d8ccb5f8
`absolute` renders better on mobile (i.e. touch-enabled) devices when zooming, `fixed` is needed on non-touch devices to show even after we’ve scrolled.

instantclick.js
@@ -3,6 +3,7 @@
 var InstantClick = function(document, location) {
   // Internal variables
   var $ua = navigator.userAgent,
+      $hasTouch = 'createTouch' in document,
       $currentLocationWithoutHash,
       $urlToPreload,
       $preloadTimer,
@@ -414,14 +415,14 @@ var InstantClick = function(document, location) {
       }
 
       var style = document.createElement('style')
-      style.innerHTML = '#instantclick{position:absolute;top:0;left:0;width:100%;pointer-events:none;z-index:3000;' + transitionProperty + ':opacity .25s .1s}'
+      style.innerHTML = '#instantclick{position:' + ($hasTouch ? 'absolute' : 'fixed') + ';top:0;left:0;width:100%;pointer-events:none;z-index:3000;' + transitionProperty + ':opacity .25s .1s}'
         + '.instantclick-bar{background:#29f;width:100%;margin-left:-100%;height:3px;' + transitionProperty + ':all .25s}'
       /* We set the bar's background in `.instantclick-bar` so that it can be
          overriden in CSS with `#instantclick-bar`, as IDs have higher priority.
       */
       document.head.appendChild(style)
 
-      if ('orientation' in window) {
+      if ($hasTouch) {
         updatePositionAndScale()
         addEventListener('resize', updatePositionAndScale)
         addEventListener('scroll', updatePositionAndScale)