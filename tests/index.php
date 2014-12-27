f777fc490afea507a12abf5fa8b3134c18da7510
Alex Dieulot <adieulot@gmail.com>
2014-12-27 20:50:33 +0100

    Remove cruft, add comments

--- tests/index.php
+++ tests/index.php
@@ -1,6 +1,6 @@
 <?
 $pages = [
-  'Index page' => '',
+  'Index page' => 'index',
   'Page with anchors #1' => 'anchor1',
   'Page with anchors #2' => 'anchor2',
   'Page without title' => 'no-title',
@@ -11,38 +11,42 @@ $pages = [
   'Alter on receive' => 'alter-receive',
 ];
 
-$page = 'index';
 if (isset($_GET['page']) && in_array($_GET['page'], $pages)) {
   $page = $_GET['page'];
+}
+else {
+  $page = 'index';
 }
 
 $delays = [200, 500, 1000, 2000, 10000];
 
-$preload_on = 'hover';
-if (isset($_GET['on'])) {
-  if ((int)$_GET['on'] != 0) {
-    $preload_on = (int)$_GET['on'];
-  }
-  elseif ($_GET['on'] == 'mousedown') {
-    $preload_on = 'mousedown';
-  }
+if (isset($_GET['on']) && (int)$_GET['on'] != 0) {
+  $preload_on = (int)$_GET['on'];
+}
+elseif (isset($_GET['on']) && $_GET['on'] == 'mousedown') {
+  $preload_on = 'mousedown';
+}
+else {
+  $preload_on = 'hover';
 }
 
 $nocache = '&amp;nocache=' . microtime(true) * 10000;
 $append = '&amp;on=' . $preload_on . $nocache;
 
 if (isset($_GET['wait'])) {
-   usleep((int)$_GET['wait'] * 1000);
+  usleep((int)$_GET['wait'] * 1000);
 }
 ?>
 <!doctype html>
 <meta charset="utf-8">
 <? if ($page != 'no-title'): ?>
-<title><? if ($page == 'entities'): ?>
+<title>
+<? if ($page == 'entities'): ?>
 Entities in the &#8249;title&rsaquo;
 <? else: ?>
 <?= date('H : i : s') ?> . <? printf("%03d", microtime() * 1000) ?>
-<? endif ?></title>
+<? endif ?>
+</title>
 <? endif ?>
 <link rel="stylesheet" href="style.css?<?= $append ?>">
 <meta name="viewport" content="width=600">
@@ -52,14 +56,16 @@ Entities in the &#8249;title&rsaquo;
 <style data-instant-track>body { overflow-y: scroll; }</style>
 <script data-instant-track>if (window.lol) { lol() }</script>
 
-<? if ($page == 'nprogress'): ?>
+<? // NProgress specific code
+if ($page == 'nprogress'): ?>
 <style>#instantclick { display: none; }</style>
 <link rel="stylesheet" href="vendors/nprogress/nprogress-0.1.2.css">
 <script src="vendors/jquery/jquery-2.1.0.js"></script>
 <script src="vendors/nprogress/nprogress-0.1.2.js"></script>
 <? endif ?>
 
-<? if ($page == 'minimal'): ?>
+<? // Minimal specific code
+if ($page == 'minimal'): ?>
 <body>Hiya.
 <? exit;
 endif ?>
@@ -79,14 +85,12 @@ endif ?>
 
 <? foreach ($pages as $name => $row): ?>
   <tr>
-    <td><a href="?<?= ($row != '' ? ('page=' . $row) : '') . $append ?>"><?= $name ?></a>
-<? if (in_array($row, [
- 'nprogress',
-])): ?>
+    <td><a href="?page=<?= $row . $append ?>"><?= $name ?></a>
+<? if ($row == 'nprogress'): ?>
         <a data-no-instant href="?page=<?= $row . $append ?>">↻</a>
 <? endif ?>
 <? foreach ($delays as $delay): ?>
-    <td><a href="?<?= ($row != '' ? ('page=' . $row) : '') . '&amp;wait=' . $delay . $append ?>"><small><?= $delay ?></small></a>
+    <td><a href="?page=<?= $row ?>&amp;wait=<?= $delay . $append ?>"><small><?= $delay ?></small></a>
 <? endforeach;
 endforeach ?>
 </table>
@@ -102,7 +106,8 @@ endforeach ?>
 <script src="instantclick.js.php?<?= $nocache ?>" data-no-instant></script>
 
 
-<? if ($page == 'nprogress'): ?>
+<? // NProgress specific code
+if ($page == 'nprogress'): ?>
 <script data-no-instant>
 InstantClick.on('wait', function() {
   NProgress.start()
@@ -165,6 +170,5 @@ if ($preload_on == 'mousedown') {
 elseif ((int)$preload_on != 0) {
   echo $preload_on;
 }
-
-?>);
+?>)
 </script>
