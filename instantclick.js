33af39e20c08e92d0e9a07baf7e1bf23be0f7683
Joe Strommen <joe.strommen@gmail.com>
2015-02-09 12:40:42 -0600

    Add support for user callback to swap new content into the DOM

--- instantclick.js
+++ instantclick.js
@@ -23,6 +23,7 @@ var InstantClick = function(document, location) {
       $trackedAssets = [],
 
   // Variables defined by public functions
+      $changePageCallback,
       $useWhitelist,
       $preloadOnMousedown,
       $delayBeforePreload,
@@ -84,6 +85,12 @@ var InstantClick = function(document, location) {
     return false
   }
 
+  function createDocument(documentHTML) {
+    var doc = document.implementation.createHTMLDocument('')
+    doc.documentElement.innerHTML = removeNoscriptTags(documentHTML)
+	return doc
+  }
+
   function isPreloadable(a) {
     var domain = location.protocol + '//' + location.host
 
@@ -126,7 +133,11 @@ var InstantClick = function(document, location) {
   }
 
   function changePage(title, body, newUrl, scrollY) {
-    document.documentElement.replaceChild(body, document.body)
+    if ($changePageCallback) {
+      $changePageCallback(body, !!newUrl)
+    } else {
+      document.documentElement.replaceChild(body, document.body)
+    }
     /* We cannot just use `document.body = doc.body`, it causes Safari (tested
        5.1, 6.0 and Mobile 7.0) to execute script tags directly.
     */
@@ -134,23 +145,26 @@ var InstantClick = function(document, location) {
     if (newUrl) {
       history.pushState(null, null, newUrl)
 
-      var hashIndex = newUrl.indexOf('#'),
-          hashElem = hashIndex > -1
-                     && document.getElementById(newUrl.substr(hashIndex + 1)),
-          offset = 0
+      if (!$changePageCallback) {
+        var hashIndex = newUrl.indexOf('#'),
+            hashElem = hashIndex > -1
+                       && document.getElementById(newUrl.substr(hashIndex + 1)),
+            offset = 0
 
-      if (hashElem) {
-        while (hashElem.offsetParent) {
-          offset += hashElem.offsetTop
-
-          hashElem = hashElem.offsetParent
+        if (hashElem) {
+          while (hashElem.offsetParent) {
+            offset += hashElem.offsetTop
+  
+            hashElem = hashElem.offsetParent
+          }
         }
+      
+        scrollTo(0, offset)
       }
-      scrollTo(0, offset)
-
+      
       $currentLocationWithoutHash = removeHash(newUrl)
     }
-    else {
+    else if (!$changePageCallback) {
       scrollTo(0, scrollY)
     }
 
@@ -288,8 +302,8 @@ var InstantClick = function(document, location) {
     $timing.ready = +new Date - $timing.start
 
     if ($xhr.getResponseHeader('Content-Type').match(/\/(x|ht|xht)ml/)) {
-      var doc = document.implementation.createHTMLDocument('')
-      doc.documentElement.innerHTML = removeNoscriptTags($xhr.responseText)
+      var documentHTML = $xhr.responseText
+      var doc = createDocument(documentHTML)
       $title = doc.title
       $body = doc.body
 
@@ -305,8 +319,7 @@ var InstantClick = function(document, location) {
 
       var urlWithoutHash = removeHash($url)
       $history[urlWithoutHash] = {
-        body: $body,
-        title: $title,
+        documentHTML: documentHTML,
         scrollY: urlWithoutHash in $history ? $history[urlWithoutHash].scrollY : 0
       }
 
@@ -681,7 +694,10 @@ var InstantClick = function(document, location) {
     }
     for (var i = arguments.length - 1; i >= 0; i--) {
       var arg = arguments[i]
-      if (arg === true) {
+      if (typeof arg === 'function') {
+        $changePageCallback = arg
+      }
+      else if (arg === true) {
         $useWhitelist = true
       }
       else if (arg == 'mousedown') {
@@ -693,8 +709,7 @@ var InstantClick = function(document, location) {
     }
     $currentLocationWithoutHash = removeHash(location.href)
     $history[$currentLocationWithoutHash] = {
-      body: document.body,
-      title: document.title,
+      documentText: document.documentElement.outerHTML,
       scrollY: pageYOffset
     }
 
@@ -736,7 +751,8 @@ var InstantClick = function(document, location) {
 
       $history[$currentLocationWithoutHash].scrollY = pageYOffset
       $currentLocationWithoutHash = loc
-      changePage($history[loc].title, $history[loc].body, false, $history[loc].scrollY)
+      var newDocument = createDocument($history[loc].documentHTML)  
+      changePage(newDocument.title, newDocument.body, false, $history[loc].scrollY)
     })
   }
 
