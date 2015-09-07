672f01a57d8c19cf74a61ec951d1fec74fbd7c89
Alex Dieulot <adieulot@gmail.com>
2015-09-07 23:13:35 +0200

    Adjust `tests/instantclick.js.php` to serve the two files

--- tests/instantclick.js.php
+++ tests/instantclick.js.php
@@ -1,8 +1,3 @@
 <?php
-$file = '../src/instantclick.js';
-if (file_exists('instanclick.js')) {
-  $file = 'instantclick.js';
-}
-
 header('Content-Type: text/javascript');
-echo file_get_contents($file);
+echo file_get_contents('../src/instantclick.js') . file_get_contents('../src/loading-indicator.js');
