259497e16b7ef9ab6cbd41d81bddfb6199153398
Alexande Dieulot <adieulot@gmail.com>
2016-10-26 23:06:37 +0200

    Revamp loading indicator design

--- src/loading-indicator.js
+++ src/loading-indicator.js
@@ -1,131 +1,87 @@
 /* InstantClick's loading indicator | (C) 2014-2015 Alexandre Dieulot | http://instantclick.io/license */
 
 ;(function() {
-  var $container
-    , $element
-    , $transformProperty
-    , $progress
+  var $element
     , $timer
-    , $hasTouch = 'createTouch' in document
 
   function init() {
-    $container = document.createElement('div')
-    $container.id = 'instantclick'
     $element = document.createElement('div')
-    $element.id = 'instantclick-bar'
-    $container.appendChild($element)
+    $element.id = 'instantclick'
 
-    var vendors = ['Webkit', 'Moz', 'O']
-
-    $transformProperty = 'transform'
-    if (!($transformProperty in $element.style)) {
-      for (var i = 0; i < vendors.length; i++) {
-        if (vendors[i] + 'Transform' in $element.style) {
-          $transformProperty = vendors[i] + 'Transform'
+    var vendors = {
+          Webkit: true,
+          Moz: true
         }
-      }
+      , vendorsPrefixes = {
+          animation: '',
+          transform: ''
+        }
+
+    function camelCase(str) {
+      return str.replace(/^(.)/, function(firstChar) {
+        return firstChar.toUpperCase()
+      })
     }
 
-    var transitionProperty = 'transition'
-    if (!(transitionProperty in $element.style)) {
-      for (var i = 0; i < vendors.length; i++) {
-        if (vendors[i] + 'Transition' in $element.style) {
-          transitionProperty = '-' + vendors[i].toLowerCase() + '-' + transitionProperty
+    for (var property in vendorsPrefixes) {
+      if (!(property in $element.style)) {
+        for (var vendor in vendors) {
+          if (vendor + camelCase(property) in $element.style) {
+            vendorsPrefixes[property] = '-' + vendor.toLowerCase() + '-'
+          }
         }
       }
     }
 
     var styleElement = document.createElement('style')
-    styleElement.innerHTML = '#instantclick{color:#29d;position:' + ($hasTouch ? 'absolute' : 'fixed') + ';top:0;left:0;width:100%;pointer-events:none;z-index:2147483647;opacity:0;' + transitionProperty + ":opacity 1s .5s}\n"
-      + '#instantclick-bar{background:currentColor;width:100%;margin-left:-100%;height:2px;' + transitionProperty + ':all 1s}'
-    document.head.insertBefore(styleElement, document.head.firstChild)
-
-    if ($hasTouch) {
-      updatePositionAndScale()
-      addEventListener('resize', updatePositionAndScale)
-      addEventListener('scroll', updatePositionAndScale)
-    }
-
-    document.body.appendChild($container)
+    styleElement.setAttribute('instantclick', '') // So that this style element doesn't surprise developers in the browser DOM inspector.
+    styleElement.innerHTML = '#instantclick {pointer-events:none; z-index:2147483647; position:fixed; top:0; left:0; width:100%; height:3px; border-radius:2px; color:hsl(192,100%,50%); background:currentColor; box-shadow: 0 -1px 4px currentColor; opacity: 0;}' +
+                             '#instantclick.visible {opacity:1; ' + vendorsPrefixes.animation + 'animation:instantclick .6s linear infinite;}' +
+                             '@' + vendorsPrefixes.animation + 'keyframes instantclick {0%,5% {' + vendorsPrefixes.transform + 'transform:translateX(-100%);} 45%,55% {' + vendorsPrefixes.transform + 'transform:translateX(0%);} 95%,100% {' + vendorsPrefixes.transform + 'transform:translateX(100%);}}'
+    document.head.appendChild(styleElement)
   }
 
-  function start() {
-    $progress = 0
-    $container.style.opacity = '0'
-    update()
-    setTimeout(display, 0) // Done in a timer to do that on next frame, so that the CSS animation happens
-    $timer = setTimeout(inc, 500)
-  }
-
-  function display() {
-    if ($progress == 100) {
-      // `done` has been called in between `start` and the execution of its timer
+  function changeListener(isInitialPage) {
+    if (!instantClick.supported) {
       return
     }
 
-    $progress = 10
-    $container.style.opacity = '1'
-    update()
-  }
-
-  function inc() {
-    $progress += 1 + (Math.random() * 2)
-    if ($progress > 99) {
-      $progress = 99
+    if (isInitialPage) {
+      init()
     }
-    else {
-      $timer = setTimeout(inc, 500)
+
+    document.body.appendChild($element)
+
+    if (!isInitialPage) {
+      hide()
     }
-    update()
   }
 
-  function update() {
-    $element.style[$transformProperty] = 'translate(' + $progress + '%)'
+  function waitListener() {
+    $timer = instantClick.setTimeout(show, 800)
   }
 
-  function done() {
-    $progress = 100
+  function show() {
+    $element.className = 'visible'
+  }
+
+  function hide() {
+    $element.className = ''
     clearTimeout($timer)
-  }
-
-  function remove() {
-    clearTimeout($timer)
-    if (document.getElementById($container.id)) {
-      document.body.removeChild($container)
-    }
-  }
-
-  function updatePositionAndScale() {
-    /* Adapted from code by Sam Stephenson and Mislav Marohnic
-       http://signalvnoise.com/posts/2407
-    */
-
-    $container.style.left = pageXOffset + 'px'
-    $container.style.width = document.body.clientWidth + 'px'
-    $container.style.top = pageYOffset + 'px'
-
-    var landscape = 'orientation' in window && Math.abs(orientation) == 90
-      , scaleY = innerWidth / screen[landscape ? 'height' : 'width'] * 1.34
-    /* We multiply the size by 2 because the progress bar is harder
-       to notice on a mobile device.
-    */
-    $container.style[$transformProperty] = 'scaleY(' + scaleY  + ')'
   }
 
 
   ////////////////////
 
 
-  instantClick.on('change', function(isInitialPage) {
-    if (isInitialPage && instantClick.supported) {
-      init()
-    }
-    else if (!isInitialPage) {
-      done()
-    }
-  })
+  instantClick.on('change', changeListener)
+  instantClick.on('wait', waitListener)
+  instantClick.on('exit', hide)
+  instantClick.on('restore', hide)
 
-  instantClick.on('wait', start)
-  instantClick.on('exit', remove)
-  instantClick.on('restore', remove) // Should be removed in a `beforechange` event instead
+
+  ////////////////////
+
+
+  instantClick.showLoadingIndicator = show
 })();
